a91e1cfcd87b05b74f77ba186cdf52c418d72042
Alexey <alexeyp0708@gmail.com>
2023-11-02 04:52:55 +0500

    AdditionalAssertionsTrait::isError - added Error handling via `try chatch`

--- src/Assertions/AdditionalAssertionsTrait.php
+++ src/Assertions/AdditionalAssertionsTrait.php
@@ -7,12 +7,15 @@ trait AdditionalAssertionsTrait
 {
     /**
      * Will return TRUE if an observed error was thrown, FALSE if there are generated other errors or no errors  , and will throw an error if other errors are observed
-     * @param\Closure $call
-     * @param string|\Closure $eql
-     * @param int $level
+     * @param\Closure $call Runs a closure with a code that needs to be checked for an error. If the error is an exception, then the variable $this will be defined in the closure. Checking for $this - isset($this)
+     * @param string|\Closure $eql If a line, then checks whether this substring is present at the beginning of the error message.
+     * If there is a closure, then the error checking code must be specified in the closure. If the test is successful, the closure should return true.
+     * @param int|\Error $level
      */
     public static function isError($call, $eql, $level = E_ALL): bool
     {
+        $err_level=is_int($level)?$level:E_ALL;
+        $exc_level=is_string($level)?$level:\Error::class;
         if (is_string($eql)) {
             $substr = $eql;
             $eql = function (...$args) use ($substr) {
@@ -24,8 +27,10 @@ trait AdditionalAssertionsTrait
         }
         $check = false;
         $bef_hand = null;
-        $bef_hand = set_error_handler(function (...$args) use (&$bef_hand, &$check, $eql) {
-            if ($eql(...$args)) {
+        $is_caught_error=false;
+        $bef_hand = set_error_handler(function (...$args) use (&$bef_hand, &$check, $eql,&$is_caught_error) {
+            $is_caught_error=true;
+            if (true===$eql(...$args)) {
                 // Expected error
                 $check = true;
                 return true;
@@ -34,9 +39,24 @@ trait AdditionalAssertionsTrait
                 return $bef_hand(...$args);
             }
             return false;
-        }, $level);
-        $call();
-        restore_error_handler();
-        return $check;
+        }, $err_level);
+        try{
+            $call();
+        }catch(\Error $e){
+            if(!($e instanceof $exc_level)){
+                throw $e;
+            }
+            $eql=$eql->bindTo($e);
+            $code=$e->getCode();
+            if(empty($code)){
+                $code=E_ERROR;
+            }
+            if(!$is_caught_error && true===$eql($code,$e->getMessage(),$e->getFile(),$e->getLine())){
+                $check=true;
+            }
+        } finally {
+            restore_error_handler();
+            return $check;
+        }
     }
 }
--- tests/Assertions/AdditionalAssertionsTest.php
+++ tests/Assertions/AdditionalAssertionsTest.php
@@ -10,6 +10,8 @@ class AdditionalAssertionsTest extends TestCase
     public function testIsError()
     {
         $this->assertTrue(Assert::isError(fn() => trigger_error('ops!'), 'ops', E_USER_NOTICE), 'test equal by errors message ');
+        $this->assertTrue(Assert::isError(fn() => trigger_error('ops!',E_USER_ERROR), 'ops', E_USER_ERROR), 'test equal by errors message ');
+
         $obj = new class {
         };
         $this->assertTrue(Assert::isError(fn() => $obj->no_prop, 'Undefined property:', E_NOTICE | E_WARNING), 'test equality by error message, for an arbitrary error');
@@ -23,12 +25,50 @@ class AdditionalAssertionsTest extends TestCase
             $str = 'Undefined property:';
             if (substr($args[1], 0, strlen($str)) === $str) {
                 $check = true;
+                return true;
             }
             return false;
-        });
+        },E_NOTICE | E_WARNING);
         $this->assertFalse(Assert::isError(fn() => $obj->no_prop, 'Ops', E_NOTICE | E_WARNING));
         restore_error_handler();
         $this->assertTrue($check, 'Generating an error that was not observed');
         unset($check);
+    }
+
+    public function testIsErrorAsException()
+    {
+       $obj= new class {
+           static $prop=1;
+       };
+        $this->assertTrue(Assert::isError(function() use($obj) {unset($obj::$prop);}, 'Attempt to unset static property', E_ERROR));
+        $this->assertTrue(Assert::isError(function() use($obj) {unset($obj::$prop);}, 'Attempt to unset static property', \Error::class));
+        $tester=$this;
+        $buf_this=null;
+        $buf_code=null;
+        $buf_message=null;
+        $buf_file=null;
+        $buf_line=null;
+        $this->assertTrue(Assert::isError(
+            function() use($obj) { unset($obj::$prop);}, 
+            function($code,$message,$file,$line) use (
+                $tester,
+                &$buf_this,
+                &$buf_code,
+                &$buf_message,
+                &$buf_file,
+                &$buf_line
+            ){
+            $buf_this=isset($this)?$this:null;
+            $buf_code=$code;
+            $buf_message=$message;
+            $buf_file=$file;
+            $buf_line=$line;
+            return true;
+        }, \Error::class));
+        $this->assertTrue($buf_this!==null && $buf_this instanceof \Error);
+        $this->assertTrue($buf_code===E_ERROR);
+        $this->assertTrue($buf_message===$buf_this->getMessage());
+        $this->assertTrue($buf_file===$buf_this->getFile());
+        $this->assertTrue($buf_line===$buf_this->getLine());
     }
 }
